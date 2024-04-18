a97c0385e4c8c7044ef14e0e75ba607216c6daa3
Santosh <santoshmjadhav1994@gmail.com>
2024-04-18 19:20:49 +0530

    blogs and search functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from blogs.models import Blog, Category
+from blogs.models import About, Blog, Category
 
 
 
@@ -9,8 +9,16 @@ def home(request):
     
     featured_posts = Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False,status='Published')
+
+    # Fetch about us 
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context={
         'featured_posts':featured_posts,
         'posts':posts,
+        'about':about,
     }
     return render(request,'home.html',context)
--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
-from blogs.models import Blog, Category
+from blogs.models import About, Blog, Category, SocialLink
 
 
 
@@ -11,6 +12,17 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ('id','title','category__category_name','status')
     list_editable = ('is_featured',)
 
+
+class AboutAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        count = About.objects.all().count()
+        if count == 0:
+            return True
+        return False
+
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
+admin.site.register(About,AboutAdmin)
+admin.site.register(SocialLink)
